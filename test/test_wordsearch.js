1892342cf3ffc763183be66825110136031ad064
Cassandra Keddis <cassandra@thinkific.com>
2022-10-21 15:26:58 -0700

    copy from repo worked on during pair programming

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,61 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'I', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'D', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'R', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FINDER');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the matrix is empty", function() {
+    const result = wordSearch([], 'FINDER');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if no word is passed", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'I', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'D', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'R', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+    assert.isFalse(result);
+  });
+
+  it("should return false if a string isn't passed", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'I', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'D', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'R', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 12345);
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let result = []; // Result will be an array of arrays
+  for (let i = 0; i < matrix[0].length; i++) {
+    let slice = matrix[0].slice(i, i + 1); //Slices each element of the first array as separate arrays
+    result.push(slice); // Push the arrays into
+  }
+  
+  for (let j = 1; j < matrix.length; j++) {
+    for (let k = 0; k < result.length; k++) {
+      result[k].push(matrix[j][k]);
     }
-}
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  let isWord = false;
+  if (!word || typeof word !== 'string') {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      isWord = true;
+    }
+
+    const vertical = transpose(letters);
+    const verticalJoin = vertical.map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+      if (l.includes(word)) {
+        isWord = true;
+      }
+    }
+
+  }
+  return isWord;
+};
+
+module.exports = wordSearch;
